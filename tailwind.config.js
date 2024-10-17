df9811e8450424e810504afb0f7393ff1e0ef3a2
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-10-17 08:44:29 +0800

    ✨ feat(tailwind): configured theme with custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "app/**/*.{js,jsx,ts,tsx}",
+      "./app/**/*.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#5faab1',
+          '50': '#f2f9f9',
+          '100': '#ddeff0',
+          '200': '#bfe0e2',
+          '300': '#92cace',
+          '400': '#5faab1',
+          '500': '#438e96',
+          '600': '#3b757f',
+          '700': '#356169',
+          '800': '#325158',
+          '900': '#2d464c',
+          '950': '#1a2c32',
+        },
+        secondary: {
+          DEFAULT: "#9ca3af",
+          '50': '#f9fafb',
+          '100': '#f3f4f6',
+          '200': '#e5e7eb',
+          '300': '#d1d5db',
+          '400': '#9ca3af',
+          '500': '#6b7280',
+          '600': '#4b5563',
+          '700': '#374151',
+          '800': '#1f2937',
+          '900': '#111827',
+          '950': '#030712',
+        }
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
