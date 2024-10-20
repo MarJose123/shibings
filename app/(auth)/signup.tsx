1e4f8f5f05db008eb4d483ec7b050d0951db5ea5
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-10-20 17:37:56 +0800

    ✨ feat(auth): implemented form validation for signup

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,80 +1,139 @@
-import {router, Stack} from "expo-router";
-import React, {useState} from "react";
-import {Text, TextInput, TouchableOpacity, View} from "react-native";
-import {Feather} from "@expo/vector-icons";
+import React, { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import Icons from "@/components/Icons";
+import { Controller, useForm } from "react-hook-form";
+import * as EmailValidator from "email-validator";
 
 export default function signUp() {
-    const [form, setForm] = React.useState({
-        name: '',
-        email: "",
-        password: "",
-    })
-    const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data: any) => console.log(data);
 
   return (
     <React.Fragment>
       <View className="container h-full justify-center bg-white px-2">
-          <View className="px-4 flex-col gap-4">
-              <Text className="text-3xl font-pextrabold">Create Account</Text>
-              <View>
-                  <Text className="text-base text-secondary-950 font-pmedium">
-                      Name
-                  </Text>
-                  <TextInput
-                      className="text-secondary-950 font-psemibold text-base h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary"
-                      value={form.name}
-                      placeholderTextColor="#7B7B8B"
-                      onChangeText={(e: any) => setForm({ ...form, name: e })}
-                  />
-              </View>
-              <View>
-                  <Text className="text-base text-secondary-950 font-pmedium">
-                      Email
-                  </Text>
-                  <TextInput
-                      className="text-secondary-950 font-psemibold text-base h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary"
-                      value={form.email}
-                      placeholderTextColor="#7B7B8B"
-                      onChangeText={(e: any) => setForm({ ...form, email: e })}
-                  />
-              </View>
-              <View>
-                  <Text className="text-base text-secondary-950 font-pmedium">
-                      PIN
-                  </Text>
-                  <View className="h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex-row items-center">
-                      <TextInput
-                          inputMode={'numeric'}
-                          keyboardType={'number-pad'}
-                          className="text-secondary-950 font-psemibold text-base flex-1"
-                          value={form.password}
-                          placeholderTextColor="#7B7B8B"
-                          secureTextEntry={showPassword}
-                          onChangeText={(e: any) => setForm({ ...form, password: e })}
-                      />
-                      <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-                          <View>
-                              {showPassword ? <Feather name="eye" size={24} color="black" /> : <Feather name="eye-off" size={24} color="black" />}
-                          </View>
-                      </TouchableOpacity>
-                  </View>
-              </View>
-              <TouchableOpacity
-                  activeOpacity={0.7}
-                  onPress={() => router.push('/signup')}
-                  className="items-center bg-primary rounded-xl min-h-[62px] justify-center"
-              >
-                  <View className="flex-row justify-between items-center">
-                      <Text className="text-white font-pextrabold ">Create Account</Text>
-                      <Icons.arrowRight
-                          className="stroke-1 h-4 w-4 stroke-white ml-5"
-                          fillClassName={"white"}
-                      />
-                  </View>
-              </TouchableOpacity>
-
+        <View className="px-4 flex-col gap-4">
+          <Text className="text-3xl font-pextrabold">Create Account</Text>
+          <View>
+            <Text className="text-base text-secondary-950 font-pmedium">
+              Name
+            </Text>
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  className={`text-secondary-950 font-psemibold text-base h-16 px-4 bg-black-100 rounded-2xl border-2 ${errors.name ? "border-red-400" : "border-black-200"} focus:border-secondary`}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholderTextColor="#7B7B8B"
+                />
+              )}
+              name="name"
+            />
+            {errors.name && errors.name?.type == "required" && (
+              <Text className="text-red-400">Name is required.</Text>
+            )}
           </View>
+          <View>
+            <Text className="text-base text-secondary-950 font-pmedium">
+              Email
+            </Text>
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+                validate: (value) => {
+                  return EmailValidator.validate(value);
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  className={`${errors.email ? "border-red-400" : "border-black-200"} text-secondary-950 font-psemibold text-base h-16 px-4 bg-black-100 rounded-2xl border-2 focus:border-secondary`}
+                  value={value}
+                  onBlur={onBlur}
+                  placeholderTextColor="#7B7B8B"
+                  onChangeText={onChange}
+                />
+              )}
+              name="email"
+            />
+            {errors.email && errors.email?.type == "required" && (
+              <Text className="text-red-400">Email is required.</Text>
+            )}
+          </View>
+          <View>
+            <Text className="text-base text-secondary-950 font-pmedium">
+              PIN
+            </Text>
+            <View
+              className={`${errors.email ? "border-red-400" : "border-black-200"} h-16 px-4 rounded-2xl border-2 border-black-200 focus:border-secondary flex-row items-center`}
+            >
+              <Controller
+                control={control}
+                rules={{
+                  required: true,
+                  maxLength: 4,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    inputMode={"numeric"}
+                    keyboardType={"number-pad"}
+                    className="text-secondary-950 font-psemibold text-base flex-1"
+                    value={value}
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    placeholderTextColor="#7B7B8B"
+                    secureTextEntry={showPassword}
+                  />
+                )}
+                name="password"
+              />
+
+              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <View>
+                  {showPassword ? (
+                    <Feather name="eye" size={24} color="black" />
+                  ) : (
+                    <Feather name="eye-off" size={24} color="black" />
+                  )}
+                </View>
+              </TouchableOpacity>
+            </View>
+            {errors.password && errors.password?.type == "required" && (
+              <Text className="text-red-400">PIN is required.</Text>
+            )}
+          </View>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={handleSubmit(onSubmit)}
+            className="items-center bg-primary rounded-xl min-h-[62px] justify-center"
+          >
+            <View className="flex-row justify-between items-center">
+              <Text className="text-white font-pextrabold ">
+                Create Account
+              </Text>
+              <Icons.arrowRight
+                className="stroke-1 h-4 w-4 stroke-white ml-5"
+                fillClassName={"white"}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     </React.Fragment>
   );
