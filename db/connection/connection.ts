d2504ee2d74fd133c792294c38807c8ecef84683
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-10-22 16:22:27 +0800

    ✨ feat(db): initialized database models

--- db/connection/connection.ts
+++ db/connection/connection.ts
@@ -1,6 +1,9 @@
-import { openDatabaseSync } from "expo-sqlite/next";
+import * as SQLite from 'expo-sqlite';
 import { drizzle } from "drizzle-orm/expo-sqlite";
+import * as schemaUser from "../model/users"
+import * as schemaBank from "../model/banks"
 
-const expo = openDatabaseSync("db.db");
+const expo = SQLite.openDatabaseSync("db.db");
 
 export const db = drizzle(expo);
+export const dbModel = drizzle(expo, {schema: {...schemaUser, ...schemaBank}});
